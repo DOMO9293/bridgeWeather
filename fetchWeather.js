aad70b5efb686300d00d22e689baf297563031ec
liftingturn <0oooceanhigh@gmail.com>
2019-08-16 19:25:46 +0900

    기본적인 동작 추가 완료. secret.js ignored처리 및 현재위치, 예보 협의 필요

--- fetchWeather.js
+++ fetchWeather.js
@@ -1,23 +1,46 @@
 function weatherBalloon( cityName ) {
-    var key = '8c5ed4c3d08a816a8f5821fa97c60239';
+  document.getElementById('citySelect').disabled = true;
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName+ '&appid=' + key)  
     .then(function(resp) { 
-        return resp.json() }) // Convert data to json
+       // console.log('json',resp.json());
+        return resp.json(); // Convert data to json
+       })
     .then(function(data) {
-      console.log(data);
+     console.log(data);
+     document.getElementById('icon').src = 'http://openweathermap.org/img/wn/'+data['weather'][0]['icon']+'@2x.png';
+     document.getElementById("loc").innerText = data['name'];
+     document.getElementById("temperature").innerText = Math.round(data['main']['temp'])/10+'°C';
+      let now = new Date(data['dt'] * 1000);
+     
+      console.log(now.toLocaleString('ko-KR', { hour: 'numeric', minute: 'numeric', hour12: true })) ;
+     document.getElementById('time').innerText ='측정시각 : '+ now.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+      
+      
+     
+     document.getElementById('minmaxtemp').innerText = 
+     Math.round(data['main']['temp_min'])/10+'/'+Math.round(data['main']['temp_max'])/10;
     })
+     
     .catch(function() {
       // catch any errors
     });
+    document.getElementById('citySelect').disabled = false;
   }
   
-//   window.onload = function() {  //아마 기본적으로 처음 화면 구성 내용인듯.
-//     weatherBallon( 6167865 );
-//   }
-
-  document.getElementById("fetchButton").addEventListener("click", function(){
-    weatherBalloon('Seoul');
-    alert('hey!');
-  });
+  //  window.onload = function() {  //아마 기본적으로 처음 화면 구성 내용인듯.
+  //    weatherBallon( 'Seoul' );
+  //  }
+window.onload=function(){   // 스크립트태그 위치가 위에 있어서 load완료시 작동 하도록.
+  document.getElementById('citySelect').addEventListener('change',function(){
+    weatherBalloon(this.value);
+    console.log(this.value);  // this.innerText 하면 main에서 id는 따로 안줘도 될 것 같은데 this는 select node로 넘어옴. 
+  })
+  document.getElementById('refresher').addEventListener('click',function(){
+    alert(document.getElementById('loc').innerText+'의 날씨정보를 갱신합니다');
+    weatherBalloon(document.getElementById('loc').innerText); 
+  })
+  weatherBalloon(document.getElementById('citySelect').children[0].innerText) // 처음 화면 구성 시 기본 location기준 로딩
+  
+}
 
   // f1 branch test code
